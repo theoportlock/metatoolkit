bac6e7384825831b3a0822d7117247fca0ebeaff
theoportlock <zn.tportlock@gmail.com>
2024-03-26 12:41:43 +1300

    Minor changes to compile

--- manuscript_template/compile.sh
+++ manuscript_template/compile.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-fullnam="*.tex"
-nam=$(echo $fullnam | cut -f 1 -d  '.')
-pdflatex -shell-escape $fullnam && 
-makeindex -s $nam.ist -t $nam.glg -o $nam.gls $nam.glo &&
-bibtex $nam && 
-pdflatex -shell-escape $fullnam && 
-pdflatex -shell-escape $fullnam &
+pdflatex -shell-escape main.tex &&
+makeindex -s main.ist -t main.glg -o main.gls main.glo &&
+bibtex main && 
+#biber main &&
+pdflatex -shell-escape main.tex &&
+pdflatex -shell-escape main.tex
--- manuscript_template/htmlcompile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-htlatex main.tex
-make4ht -ue mybuild.mk4 *.tex
-#soffice --convert-to docx *.html
