46a0957c5049a4654887b448a56ebb7b36b4340f
theoportlock <zn.tportlock@gmail.com>
2025-06-05 19:56:33 +1200

    More scripts incoming

--- metatoolkit/splitter.py
+++ metatoolkit/splitter.py
@@ -1,35 +1,64 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import argparse
 import pandas as pd
 
+
+def load(subject):
+    """Load TSV from absolute path."""
+    return pd.read_csv(subject, sep='\t', index_col=0)
+
+
+def save(df, output_dir, filename, index=True):
+    """Save dataframe to absolute output path inside output_dir/filename.tsv"""
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, f'{filename}.tsv')
+    df.to_csv(output_path, sep='\t', index=index)
+
+
 def splitter(df, df2, column):
+    """Split df into sub-dataframes based on unique values in df2[column]."""
     output = {}
     if df2 is None:
         df2 = df.copy()
     for level in df2[column].unique():
-        merge = df.loc[:, df.columns[df.columns != column]].join(df2.loc[df2[column] == level, column], how='inner').drop(column, axis=1)
-        output[level] = merge
+        merged = df.loc[:, df.columns[df.columns != column]].join(
+            df2.loc[df2[column] == level, column], how='inner'
+        ).drop(columns=[column])
+        output[level] = merged
     return output
 
-parser = argparse.ArgumentParser(description='''
-Splitter - splits dataframes according to the values in a defined column''')
-parser.add_argument('subject')
-parser.add_argument('column')
-parser.add_argument('--df2', required=False)
 
-known, unknown = parser.parse_known_args()
-known = {k: v for k, v in vars(known).items() if v is not None}
-unknown = eval(unknown[0]) if unknown != [] else {}
+def parse_args():
+    parser = argparse.ArgumentParser(description='''
+    Splitter - splits dataframes according to the values in a defined column''')
+    parser.add_argument('subject', help='TSV file path')
+    parser.add_argument('column', help='Column name to split by')
+    parser.add_argument('--df2', help='Optional second TSV file or basename')
+    parser.add_argument('--outdir', help='Optional output directory (absolute or relative)')
+    return parser.parse_args()
 
-df = load(known.get('subject'))
-df2 = load(known.get('df2')) if known.get("df2") else None
-col = known.get('column')
-subject = known.get('subject')
 
-output = splitter(df, df2, col)
-print(output)
+def main():
+    args = parse_args()
+    subject = args.subject
+    column = args.column
 
-for level in output:
-    save(output[level], f'{subject}{col}{level}')
+    df = load(subject)
+    df2 = load(args.df2) if args.df2 else None
+
+    output = splitter(df, df2, column)
+
+    output_dir = os.path.abspath(args.outdir)
+
+    for level, split_df in output.items():
+        save(split_df, output_dir, level)
+
+    print(f"✅ Split files saved to: {output_dir}")
+
+
+if __name__ == '__main__':
+    main()
+
